c94db2450d708260c9621ffafe2c92be9ea8ab26
RathgebL <108350061+RathgebL@users.noreply.github.com>
2022-09-23 21:13:31 +0200

    chore: Scaffold hatch project

---
+++ src/ConventionalChangelog/main.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2022-present Lennart Rathgeb <108350061+RathgebL@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+
+
+def main() -> None:
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2022-present Lennart Rathgeb <108350061+RathgebL@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
